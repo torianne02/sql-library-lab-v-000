793d3758d2538940fde6efadd759b8c1d28004f3
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-06-28 21:18:28 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,3 +2,28 @@ INSERT INTO series (title, author_id, subgenre_id)
   VALUES ("Twilight", 1, 1);
 INSERT INTO series (title, author_id, subgenre_id)
   VALUES ("The Spiderwick Chronicles", 2, 2);
+
+INSERT INTO subgenres (name)
+  VALUES ("Vampires and Werewolfs");
+INSERT INTO subgenres (name)
+  VALUES ("Trolls, Pixies, Sprites, etc.");
+
+INSERT INTO authors (name)
+  VALUES ("Stephenie Meyer");
+INSERT INTO authors (name)
+  VALUES ("Holly Black");
+
+INSERT INTO books (title, year, series_id)
+  VALUES ("Twilight", 2008, 1);
+INSERT INTO books (title, year, series_id)
+  VALUES ("The Twilight Saga: New Moon", 2009, 1);
+INSERT INTO books (title, year, series_id)
+  VALUES ("The Twilight Saga: Eclipse", 2010, 1);
+INSERT INTO books (title, year, series_id)
+  VALUES ("The Field Guide", 2003, 2);
+INSERT INTO books (title, year, series_id)
+  VALUES ("The Seeing Stone", 2003, 2);
+INSERT INTO books (title, year, series_id)
+  VALUES ("Lucinda's Secret", 2003, 2);
+
+
