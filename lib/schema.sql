efe5d1ca3e9ba017385ade02a2dce4d42fe73ea9
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-06-28 21:00:25 +0000

    automatically backed up by learn

---
+++ lib/schema.sql
@@ -0,0 +1,32 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author TEXT,
+  sub-genre TEXT
+);
+
+CREATE TABLE subgenre (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER,
+  series_id INTEGER
+);
