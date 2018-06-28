7e53c925ae01e13cdf8989ed0cbbb5b9ef138467
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-06-28 22:11:08 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,5 +34,8 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT c.name, SUM(*)
+  FROM character_books AS cb
+  JOIN characters AS c ON c.id = cb.character_id
+  GROUP BY c.name;"
 end
