04e6337d7e64814bbd601ef4fb91dd86786099ce
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-06-28 21:36:49 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -42,3 +42,36 @@ INSERT INTO characters (name, motto, species, author_id, series_id)
   VALUES ("Hogsqueal", "I must destroy the ogre who killed my family.", "Hobgoblin", 2, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id)
   VALUES ("Thimbletack", "Yummy yummy yummy.", "Faerie", 2, 2);
+
+INSERT INTO character_books (book_id, character_id)
+  VALUES (1, 1);
+INSERT INTO character_books (book_id, character_id)
+  VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id)
+  VALUES (1, 3);
+INSERT INTO character_books (book_id, character_id)
+  VALUES (2, 1);
+INSERT INTO character_books (book_id, character_id)
+  VALUES (2, 2);
+INSERT INTO character_books (book_id, character_id)
+  VALUES (3, 1);
+INSERT INTO character_books (book_id, character_id)
+  VALUES (3, 2);
+INSERT INTO character_books (book_id, character_id)
+  VALUES (3, 4);
+INSERT INTO character_books (book_id, character_id)
+  VALUES (4, 5);
+INSERT INTO character_books (book_id, character_id)
+  VALUES (4, 6);
+INSERT INTO character_books (book_id, character_id)
+  VALUES (4, 7);
+INSERT INTO character_books (book_id, character_id)
+  VALUES (5, 5);
+INSERT INTO character_books (book_id, character_id)
+  VALUES (5, 6);
+INSERT INTO character_books (book_id, character_id)
+  VALUES (5, 8);
+INSERT INTO character_books (book_id, character_id)
+  VALUES (6, 5);
+INSERT INTO character_books (book_id, character_id)
+  VALUES (6, 6);
