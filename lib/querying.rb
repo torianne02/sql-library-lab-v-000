40f82b20629a4572fad900607e9d510e819a3b95
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-06-28 21:59:37 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,9 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT a.name, sg.name
+  FROM authors AS a, subgenres AS sg
+  LEFT JOIN series AS s ON s.author_id = a.id, s.subgenre_id = sg.id;"
 end
 
 def select_series_title_with_most_human_characters
