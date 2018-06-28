2428b1cf3cb66fccd55de47177b2eff5923d9957
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-06-28 21:48:14 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -7,7 +7,9 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT c.name, c.motto
+  FROM characters AS c
+  ORDER BY c.motto_length;"
 end
 
 
